bd5c87dde88746442df42f322b587f1b8674d998
necordu <necordu_3@mail.ru>
2020-02-23 14:01:02 +0300

    initial commit

---
+++ Pods/Target Support Files/Pods-exerciseApp/Pods-exerciseApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_exerciseApp : NSObject
+@end
+@implementation PodsDummy_Pods_exerciseApp
+@end
---
+++ exerciseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exerciseApp
+//
+//  Created by Егор Редько on 23.02.2020.
+//  Copyright © 2020 necordu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
